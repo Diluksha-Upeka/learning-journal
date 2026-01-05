5ca3166e0110479ef21e588f1029d93cced6804a
Diluksha Upeka <dilukshaupeka@gmail.com>
2026-01-05 15:53:47 +0530

    Day02-Lists, Tuples and Sets

---
+++ Day_02/day02_lists.py
@@ -0,0 +1,68 @@
+# Lists
+# A list is a mutable ordered collection of items. (can be changed after creation)
+
+modules = ['Control systems', 'DSA', 'Python', 'Web Development']
+
+print(modules)
+# Accessing elements
+print(modules[0])  # First element
+print(modules[-1])  # Last element
+
+# adding elements
+modules.append('Machine Learning')
+print(modules)
+
+modules.insert(2, 'Data Science')
+print(modules)
+
+# slicing
+print(modules[1:4])  # Elements from index 1 to 3
+print(modules[:3])   # First three elements
+print(modules[2:])   # From index 2 to the end
+
+# Extending lists
+modules_2 = ['Cybersecurity', 'Cloud Computing']
+modules.extend(modules_2)
+print(modules)
+print(modules[-1])  # Last element after extending
+
+# Removing elements
+# modules.remove('DSA')
+# print(modules)
+
+popped_module = modules.pop()  # Removes last element
+print(popped_module)
+print(modules)
+
+#sorting
+# modules.reverse()
+# print(modules)
+
+modules.sort()
+print(modules)
+
+nums = [5, 2, 9, 1, 5, 6]
+nums.sort()  # for descending order use nums.sort(reverse=True)
+print(nums)
+
+# When you don't want to modify the original list
+sorted_modules = sorted(modules)  # returns a new sorted list
+print(sorted_modules)
+
+print(min(nums))
+print(max(nums))
+print(sum(nums))
+
+# Finding the index of an element
+print(modules.index('DSA'))
+print('DSA' in modules)  # Check if 'DSA' is in the list
+
+# Iterating through a list
+for index, item in enumerate(modules, start=1):  # enumerate returns a tuple of index and value for each item in the modules:
+    print(index,item) 
+
+module_str = ' -  '.join(modules)  # Joining list elements into a string
+print(module_str)
+
+new_list = module_str.split(' -  ')  # Splitting string back into a list
+print(new_list)
---
+++ Day_02/day02_tuples.py
@@ -0,0 +1,58 @@
+# TUPLES
+# A tuple is an immutable ordered collection of items. (can't be changed after creation)
+
+import sys
+import timeit
+
+modules_1 = ['a', 'b', 'c', 'd', 'e']
+modules_2 = modules_1  # Both variables point to the same list
+print(modules_1)
+print(modules_2)
+
+tuple_1 = ('a', 'b', 'c', 'd', 'e')
+tuple_2 = tuple_1  # Both variables point to the same tuple
+print(tuple_1)
+print(tuple_2)
+
+# tuple_1[0] = 'z'  # This will raise an error because tuples are immutable
+
+# NO methods like append, insert, remove, pop, sort, reverse for tuples
+
+# Size comparison
+print(sys.getsizeof(modules_1))  # Size of modules_1 in bytes
+print(sys.getsizeof(tuple_1))  # Size of tuple_1 in bytes
+
+# Speed comparison
+lists_time =timeit.timeit(stmt = "modules = ['a', 'b', 'c', 'd', 'e']", number = 1000000)
+tuples_time = timeit.timeit(stmt = "tuples = ('a', 'b', 'c', 'd', 'e')", number = 1000000)
+
+if lists_time < tuples_time:
+    print(f"Lists are faster: {lists_time} seconds")
+else:
+    print(f"Tuples are faster: {tuples_time} seconds")
+
+
+# SETS - Unordered collection of unique elements with no duplicates
+cs_courses = {'DSA', 'Python', 'Web Development', 'Python'}  # 'Python' will be stored only once
+print(cs_courses)
+
+# membership test - sets are optimized for this
+print('DSA' in cs_courses)  # True
+print('Machine Learning' in cs_courses)  # False
+
+bio_courses = {'Biology', 'Chemistry', 'Physics', 'DSA', 'Python'}
+# set operations
+print(cs_courses.intersection(bio_courses))  # common elements
+print(cs_courses.union(bio_courses))  # all unique elements
+print(cs_courses.difference(bio_courses))  # elements in cs_courses but not in bio_courses
+print(bio_courses.difference(cs_courses))  # elements in bio_courses but not in cs
+
+# Empty list, tuple, set
+empty_list = []
+empty_list = list()
+
+empty_tuple = ()
+empty_tuple = tuple()
+
+empty_set = {}  # This creates an empty dictionary, not a set
+empty_set = set()
